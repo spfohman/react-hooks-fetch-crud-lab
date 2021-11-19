d13fe77ad9ca7031b2f104c36e77739a51383f94
+ <+>
2021-11-19 12:36:17 -0500

    Done.

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -20,6 +20,24 @@ function QuestionForm(props) {
   function handleSubmit(event) {
     event.preventDefault();
     console.log(formData);
+    const newQuestion={
+      prompt: formData.prompt,
+      answers: [
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4,
+      ],
+      correctIndex: formData.correctIndex,
+    }
+    fetch('http://localhost:4000/questions', {
+      method: "POST",
+      headers:{
+        "Content-Type":"application/json",
+      },
+      Body:JSON.stringify(newQuestion),
+    })
+    
   }
 
   return (
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import QuestionItem from './QuestionItem';
 
 function QuestionList() {
+  const [questions, setQuestions]=useState([])
+  useEffect(()=>{
+    fetch('http://localhost:4000/questions')
+    .then((response)=>response.json())
+    .then((questions)=> setQuestions(questions))
+  }, [])
+
+  function handleDeleteClick(id){
+    fetch(`http://localhost:4000/questions${id}`,{
+      method: "DELETE",
+  })
+    .then((response)=>response.json())
+    .then(()=>{
+      const deletedQuestion = questions.filter((question)=>question.id !== id);
+        setQuestions(deletedQuestion)
+      })
+    }
+  
+  function handleAnswerChange(id, correctIndex){
+    fetch(`http://localhost:4000/questions${id}`,{
+      method:"PATCH",
+      headers:{
+        "Content-Type":"application/json",
+      },
+      body: JSON.stringify({correctIndex}),
+    })
+    .then((response)=>response.json())
+    .then((updatedQuestion)=>{
+      const updatedQuestions = questions.map((question)=>{
+        if(question.id===updatedQuestion.id) return updatedQuestion;
+        return question;
+      });
+      setQuestions(updatedQuestions)
+    })
+  }
+  
+  const eachQuestion = questions.map((e)=>(
+       <QuestionItem key={e.id} question={e} onDelete={handleDeleteClick} onAnswerChange={handleAnswerChange} />
+  ));
+  
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{eachQuestion}</ul>
     </section>
   );
 }
